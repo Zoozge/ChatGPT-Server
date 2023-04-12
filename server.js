573552e334b8fc98233684b9fb43893405313c36
Zoozge <130520017+Zoozge@users.noreply.github.com>
2023-04-12 06:51:41 +0300

    Connect to ChatGPT

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
 var express = require("express");
+var request = require('request');
 var app = express();
 
 const HTTP_PORT = process.env.PORT | 8080;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.get("/:text", (req, res) => {
+  var options = {
+    'method': 'POST',
+    'url': 'https://api.openai.com/v1/chat/completions',
+    'headers': {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+    },
+    body: JSON.stringify({
+      "model": "gpt-3.5-turbo",
+      "messages": [
+        {
+          "role": "user",
+          "content": req.params.text
+        }
+      ]
+    })
+  };
+
+  request(options, function (error, response) {
+    if (error)
+      throw new Error(error);
+      
+    res.send(JSON.parse(response.body).choices[0].message.content);
+  });
 });
 
 app.listen(HTTP_PORT);
