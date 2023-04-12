1f78be48fd6bae083f18c5bc72ae7a173a087078
Zoozge <130520017+Zoozge@users.noreply.github.com>
2023-04-12 08:43:33 +0300

    Change model to get faster responses

--- server.js
+++ server.js
@@ -7,19 +7,15 @@ const HTTP_PORT = process.env.PORT | 8080;
 app.get("/:text", (req, res) => {
   var options = {
     'method': 'POST',
-    'url': 'https://api.openai.com/v1/chat/completions',
+    'url': 'https://api.openai.com/v1/completions',
     'headers': {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
     },
     body: JSON.stringify({
-      "model": "gpt-3.5-turbo",
-      "messages": [
-        {
-          "role": "user",
-          "content": req.params.text
-        }
-      ]
+      "model": "text-davinci-003",
+      "prompt": req.params.text,
+      "max_tokens": 512
     })
   };
 
@@ -27,7 +23,7 @@ app.get("/:text", (req, res) => {
     if (error)
       throw new Error(error);
       
-    res.send(JSON.parse(response.body).choices[0].message.content);
+    res.send(JSON.parse(response.body).choices[0].text);
   });
 });
 
